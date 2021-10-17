d047b58314b65d217dc29cbe8202cec18cb5dfa0
HARISHKUMAR N <88447060+HARISHKUMAR023@users.noreply.github.com>
2021-10-17 09:07:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+$("h2").click(function() {
+	$("h2").css("color","white")
+	// body...
+}) ;
+$("h3").click(function() {
+	$("h3").css("color","red")
+	// body...
+}) ;
+//alert("This web page is only 50% completed.(note: by clicking ok you can view this web side) ")
+$("#Home").click(function(){
+	$("#Home").css("color","darkred")
+})
